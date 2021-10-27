e5f739baf2ac802e073bdafa32494822a10a0c1e
Anthony Sottile <asottile@umich.edu>
2021-10-27 15:19:02 -0400

    add python linter and formatter

--- main.py
+++ main.py
@@ -3,12 +3,12 @@ import argparse
 
 def main() -> int:
     parser = argparse.ArgumentParser()
-    parser.add_argument('name')
+    parser.add_argument("name")
     args = parser.parse_args()
 
-    print( f'hello hello world {args.name}' )
+    print(f"hello hello world {args.name}")
     return 0
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     exit(main())
